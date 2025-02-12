182e2a35f3cd5bd6befbaa6b0e2b4364c7fddf15
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-02-12 18:49:00 +0530

    updates

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+// datatypes
+// there 2 types of datatypes in js
+// 1. Primitive Datatypes 
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+// undefined
+car = undefined
+console.log(car); //will return undefined
+
+let car = "";    // The value is "", the typeof is "string"
+
+// 2. Non-Primitive DataTypes
+// Object:
+let person = {Name:"Anaida", Age:26};
+// Array :
+let cars = ["Saab", "Volvo", "BMW"];
+let fruits = ["apple","banana","orange"]
+var arr = [1,2.5,"Hello",{prodName:"Fan",quantity : 10},true,function(){return "I am an Array"}]
+console.log(arr)
+
+// Date :
+let currentDate = new Date();
+console.log(currentDate);
---
+++ scopes.js
@@ -0,0 +1,38 @@
+// //Scope
+// //Global scope
+// let a = "Hey morning";
+// const greet = () =>{
+//     // console.log(a);
+    
+// }
+// // greet()
+
+// //function scope
+// const hey = ()=>{
+// var a = "hello" //these wont be accessed outside this hey()
+// let b = "world"
+// // console.log(a,b);
+
+// }
+// hey()
+// // console.log(a); //this will print the variable a that is declared in gloabl scope not in fnction scope
+
+// //Block scope
+// {
+//     let greeting= "Hello world"
+//     var lang = "english"
+//     console.log(greeting);
+//     // console.log(lang);
+    
+// }
+// console.log(lang);// can access  var as it i global scoped
+// // console.log(greeting); //reference error cann't access greetig as it is block scoped
+
+//nested Scope
+// var name = "Anaida";
+// const greet = () =>{
+
+// }
+// greet()
+
+
