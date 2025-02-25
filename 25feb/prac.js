74ba1db01b288b2c565ad14c648bfaeabbd4a5d0
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-02-25 17:56:49 +0530

    update

---
+++ 25feb/prac.js
@@ -0,0 +1,17 @@
+// let str = "my-is-anaida";
+// function myfn(str){
+//     let result = ""
+//     for(let i = 0;i<str.length;i++){
+//         // console.log(str[i])
+
+//         if(str[i] === "-"){
+//             result += " "
+//         }else{
+//             result+=str[i]
+//         }
+
+//     }
+//     return result
+// }
+// console.log(myfn(str))
+
