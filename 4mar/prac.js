df763a03dc76132d88c56c9c8afe3dc6da2c3eb6
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-03-04 18:06:44 +0530

    update

---
+++ 4mar/prac.js
@@ -0,0 +1,30 @@
+// // 2. WAP to reverse an integer without converting it to a string, without using any built-in methods.
+// let a = 23;
+// const reverseInt = (a) => {
+//   let i = 0;
+//   while (a > 0) {
+//     let digit = a % 10;
+//     // console.log(digit)
+//     let reverse = 0;
+//     reverse = reverse * 10 + digit;
+
+//     i = (a / 10) | 0;
+//     // console.log(a)
+//   }
+//   return reverse
+// };
+// console.log(reverseInt(a));
+
+// let age = 26;
+
+// const myAge = (age) => {
+//   let name = "anaida";
+//   if(true){
+//     let name = "kyle"
+//     console.log (name,age);
+
+//   }
+//   // return name
+// };
+// console.log(myAge(age));
+
