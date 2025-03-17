dd9a03c6a5acb7a4e9831a44f8d4da8114526c56
Anaida <anaida.ambiliserry@nimapinfotech.in>
2025-03-17 18:05:15 +0530

    update

--- mar/17mar/prac.js
+++ mar/17mar/prac.js
@@ -1,1 +1,36 @@
-let str
+// 1
+// const bird = {
+//     size: 'small',
+//   };
+  
+//   const mouse = {
+//     name: 'Mickey',
+//     small: true,
+//   };
+// A: mouse.bird.size is not valid
+// B: mouse[bird.size] is not valid
+// C: mouse[bird["size"]] is not valid
+// D: All of them are valid
+
+// 2
+// let num = 10;
+
+// const increaseNumber = () => num++;
+// const increasePassedNumber = number => number++;
+
+// const num1 = increaseNumber();
+// const num2 = increasePassedNumber(num1);
+
+// console.log(num1);
+// console.log(num2);
+
+const value = { number: 10 };
+
+const multiply = (x = { ...value }) => {
+  console.log((x.number *= 2));
+};
+
+multiply();
+multiply();
+multiply(value);
+multiply(value);
