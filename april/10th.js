e565192626fa033e1cc453f7dd2d286bebfb51db
Anaida Ashish <anaida.ambiliserry@nimapinfotech.in>
2025-04-10 17:51:40 +0530

    update

---
+++ april/10th.js
@@ -0,0 +1,24 @@
+// console.log('Start');
+// setTimeout(() => console.log('Timeout 1'), 0);
+// Promise.resolve().then(() => console.log('Promise 1'));
+// Promise.resolve().then(() => {
+//     console.log('Promise 2');
+//     setTimeout(() => console.log('Timeout 2'), 0);
+// });
+// console.log('End');
+
+
+
+// let str = "banana"
+// function frequencyCount(str){
+//     let result = {};
+//     for(let i = 0;i<str.length;i++){
+//         let char = str[i]
+//         // console.log(char)
+//       result[char] =result[char] ? result[char]+1 : 1
+//     }
+//     return result
+// }
+// console.log(frequencyCount(str))
+
+
