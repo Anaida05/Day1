a59315a61eea65c7976bcfed6296b51a98fae273
Anaida Ashish <anaida.ambiliserry@nimapinfotech.in>
2025-04-21 18:34:43 +0530

    update

---
+++ april/21.js
@@ -0,0 +1,34 @@
+// max of three
+// let a = [10,3,232]
+// function maxOfThree(a){
+//     max = a[0]
+//     for(let i = 0;i < a.length;i++){
+//         if(a[i]>max){
+//             max=a[i]
+//         }
+//     }
+//     return max;
+// }
+// console.log(maxOfThree(a))
+
+// reverse a string
+// let str = "anaida";
+// function reverseStr(s){
+// let reversed = ""
+// for(let i=s.length-1;i>=0;i--){
+//     reversed += s[i]
+// }
+// return reversed
+// }
+// console.log(reverseStr(str))
+
+// remove duplicate
+// arr = [1,232,321,4221,14,1,4];
+// const mySet = new Set();
+// function removeDuplicate(arr,mySet){
+// for(let i = 0;i<arr.length;i++){
+//     mySet.add(arr[i])
+// }
+// return mySet
+// }
+// console.log(removeDuplicate(arr,mySet))
