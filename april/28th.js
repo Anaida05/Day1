1c6ff73196058975d7d243a1b6c2e2797407a100
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-04-28 18:00:43 +0530

    update

---
+++ april/28th.js
@@ -0,0 +1,31 @@
+// Count Vowels in a String
+// let str = "anaida-ambilissery"
+// const countVowel=(str)=>{
+//     let count = 0;
+//     for(let i = 0;i<str.length;i++){
+//         if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o'|| str[i] === 'u'){
+//             count++
+//         }
+//     }return count;
+// }
+// console.log(countVowel(str))
+
+
+// Find the Longest Word in a Sentence
+// Given a sentence, return the longest word.
+// Example: "I love programming" → "programming"
+
+// let word = "I love programming"
+// const longest = (word)=>{
+//     let largest;
+//     let output = word.split(" ")
+//     // console.log(output)
+//     for(let i = 0;i<output.length;i++){
+//          largest = output[0]
+//         if(output[i] > largest){
+//             largest = output[i]
+//         }
+//     }    
+//     return largest
+// }
+// console.log(longest(word))
