562fe1ec9b874f387240ee7020d55f24b1fcd65e
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-04-30 17:56:06 +0530

    update

---
+++ april/30th.js
@@ -0,0 +1,29 @@
+// function replaceChar(){
+//     let str = "apple";
+//     let char = "p"
+//     let replaceChar = "b"
+//     let output=""
+//     for(let i=0;i<str.length;i++){
+//         if(str[i]===char){
+//             output+= replaceChar
+//         }else{
+//             output+=str[i]
+//         }
+//     }
+//     return output
+// }
+// console.log(replaceChar())
+
+// Check if a string contains only digits
+// Input: "12345" → Output: true
+// Input: "123a5" → Output: false
+// let str="123a45"
+// function check(str) {
+//     for (let i = 0; i < str.length; i++) {
+//         if (isNaN(parseInt(str[i]))) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// console.log(check(str))
