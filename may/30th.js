51e9ea959cdb944eef9a0bac0b6c7686e963b88a
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-05-30 18:00:42 +0530

    update

---
+++ may/30th.js
@@ -0,0 +1,54 @@
+// console.log(1 + "2" + "2");
+// console.log(1 + +"2" + "2");
+// console.log(1 + -"1" + "2");
+// console.log(+"1" + "1" + "2");
+
+// let obj = { a: 1 };
+// const ref = obj;
+
+// obj = null;
+
+// console.log(ref.a);
+
+// let a = [1, 2];
+// let b = [1, 2];
+
+// console.log(a == b);
+// console.log(JSON.stringify(a) === JSON.stringify(b));
+
+// console.log("Start");
+
+// setTimeout(() => {
+//   console.log("Timeout");
+// }, 0);
+
+// Promise.resolve().then(() => {
+//   console.log("Promise");
+// });
+
+// console.log("End");
+
+// let x = [1, 2, 3];
+// x[10] = 11;
+// console.log(x.length);
+
+// console.log(NaN === NaN); 
+// console.log(Object.is(NaN, NaN));
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 0);
+// }
+
+// console.log(typeof null);
+// console.log(null instanceof Object);
+
+// let a = 1;
+// let b = a++;
+// let c = ++a;
+
+// console.log(a, b, c);
+
+// console.log([] + []); 
+// console.log([] + {}); 
+// console.log({} + []); 
+// console.log({} + {}); 
