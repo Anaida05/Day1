414f0d5162617fe2be8878ca719c035caa6118eb
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-05-31 10:29:21 +0530

    updaate

---
+++ may/31st.js
@@ -0,0 +1,15 @@
+// let num = 123;
+// function sumOfNum(num){
+//     let sum = 0;
+//     while(num > 0){
+//         let digit = num%10
+//         sum += digit
+//         num = Math.floor(num/10)
+//         console.log(num,"num")
+//     }
+//     return sum
+// }
+//  console.log(sumOfNum(num))
+
+
+
