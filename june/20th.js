a519728800cbaf55743aa05fc38160d3f24a06c8
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-06-20 18:08:14 +0530

    update

---
+++ june/20th.js
@@ -0,0 +1,58 @@
+// console.log("start")
+
+// new Promise((res, rej)=>{
+//     console.log("Initialized")
+//     setTimeout(()=>{
+//         res("resolved")
+//     }, 1000)
+// }).then(result=>{
+//     console.log(result)
+// })
+// console.log("end")
+
+// null === null
+
+// print array from 1 to 50
+// let n = 50;
+// function printArr(n){
+//   let a = [];
+//   for(let i = 1;i<=n;i++){
+//       a.push(i)
+//   }
+//   return a
+// }
+// console.log(printArr(n))
+
+// if num is divisible by 3 : print("Hii")
+// let n = 10;
+// function byThree(n){
+//     let a = []
+//    for(let i = 1;i<=n;i++){
+//        if(i%3 === 0){
+//           a.push("Hii") 
+//        }else{
+//            a.push(i)
+//        }
+//    } 
+//    return a
+// }
+// console.log(byThree(n))
+
+// let my = {
+//     a : 10,
+//     x : function(){
+//         console.log(this.a)
+//     }
+// }
+// my.x()
+
+
+// console.log('Start');
+// setTimeout(() => console.log('Timeout 1'), 0);
+// Promise.resolve().then(() => console.log('Promise 1'));
+// Promise.resolve().then(() => {
+//     console.log('Promise 2');
+//     setTimeout(() => console.log('Timeout 2'), 0);
+// });
+// console.log('End');
+// console.log("start")
