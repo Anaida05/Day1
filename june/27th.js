8fe3501212fcd2d0fcb3d2bdaed8cb23b68a13c1
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-06-27 18:06:17 +0530

    update

---
+++ june/27th.js
@@ -0,0 +1,45 @@
+// function factorial(n) {
+//   if (n === 0 || n === 1) return 1;
+//   return n * factorial(n - 1);
+// }
+
+// console.log(factorial(5)); 
+
+
+
+// function fibonacci(n) {
+//   let result = [0, 1];
+//   for (let i = 2; i < n; i++) {
+//     result.push(result[i - 1] + result[i - 2]);
+//   }
+//   return result.slice(0, n);
+// }
+
+// console.log(fibonacci(6)); 
+
+
+
+
+// function sumArray(arr) {
+//   return arr.reduce((acc, val) => acc + val, 0);
+// }
+
+// console.log(sumArray([1, 2, 3, 4])); 
+
+
+
+// function countVowels(str) {
+//   return (str.match(/[aeiou]/gi) || []).length;
+// }
+
+// console.log(countVowels("hello"));
+
+
+
+// function hasDuplicates(arr) {
+//   return new Set(arr).size !== arr.length;
+// }
+
+// console.log(hasDuplicates([1, 2, 3, 4])); 
+// console.log(hasDuplicates([1, 2, 2, 3])); 
+
