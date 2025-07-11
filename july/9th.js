134c2cc1ed132b2552149c34a5b911a7bf4c9a39
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-07-11 18:06:55 +0530

    update

---
+++ july/9th.js
@@ -0,0 +1,49 @@
+// const add=(a)=>{
+//     return (b)=>{
+//         return (c)=>{
+//             return a + b + c
+//         }
+//     }
+// }
+// console.log(add(2)(3)(5))
+
+
+// const strConcat=(str1)=>{
+//     return (str2)=>{
+//         return str1+" "+str2
+//     }
+// }
+// console.log(strConcat("Hello")("Anaida"))
+
+
+//  for(var i=0; i<10; i++){
+//      setTimeout(()=>{
+//          console.log(i);
+//          },1000) }
+
+
+// function outer(){ 
+//     var b = 2 
+//     function inner(){
+//         b++;
+//         console.log(b)
+//         var b = 3;
+//         }
+//         inner();
+//         }
+//         outer();
+
+
+//  (function () {
+//      try {
+//          throw new Error(); 
+//      } catch (x) { 
+//          var x = 1, 
+//          y = 2; 
+//          console.log(x); 
+//      } 
+//      console.log(x);
+//      console.log(y); 
+//  })()
+ 
+
