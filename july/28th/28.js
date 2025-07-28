d5e33d3ee5b126559980443678b7c9f028f00fce
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-07-28 17:59:28 +0530

    update

---
+++ july/28th/28.js
@@ -0,0 +1,26 @@
+// let str = "background-color"
+// function convertStr(s) {
+//     let val = s.split("-")
+//     for(let i = 1;i<val.length;i++){
+//         val[i] = val[i].charAt(0).toUpperCase() + val[i].slice(1)
+//     }
+//     return val.join("")
+// }
+// console.log(convertStr(str))
+
+
+// let str = "anaida";
+// function rotate(s){
+//    let k = 2 
+//    let len = s.length;
+//    let output = "";
+   
+//    for(let i = len - k;i<s.length;i++){
+//        output+=s[i]
+//    }
+//    for(let i = 0;i<len-k;i++){
+//        output+=s[i]
+//    }
+//    return output
+// }
+// console.log(rotate(str))
