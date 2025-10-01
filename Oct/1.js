6311477ecf69d286c4aec4d18775ec55de4f45ab
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-10-01 17:52:59 +0530

    update

---
+++ Oct/1.js
@@ -0,0 +1,24 @@
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function sumArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+
+function sumArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
