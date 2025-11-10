6c83fd0e281c8fd92a22b91ef60acbe6e85dbbb8
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-11-10 18:01:26 +0530

    update

---
+++ Nov/10.js
@@ -0,0 +1,13 @@
+// Move Zeroes
+function moveZeroes(nums) {
+  let pos = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[pos], nums[i]] = [nums[i], nums[pos]];
+      pos++;
+    }
+  }
+  return nums;
+}
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
