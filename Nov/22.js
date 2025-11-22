5b0db694bc14e9a91d75a693dec076ff73800f1d
Anaida05 <anaida.ambiliserry@nimapinfotech.in>
2025-11-22 10:21:35 +0530

    typescript syntaxes

---
+++ Nov/22.js
@@ -0,0 +1,40 @@
+//type
+// const age : number = 20
+// console.log(age)
+
+
+//interface
+// interface User {
+//     name : string,
+//     age : number
+// }
+
+// const abc : User = {
+//     name : "anaida",
+//     age : 26
+// }
+// console.log(abc)
+
+//generic
+// const world=<T>(value : T) :T=>{
+//     return value
+// }
+// console.log(world("string"))
+// console.log(world(10))
+// console.log(world(true))
+
+//inference
+// let age = 10
+// age = "anaida"
+// console.log(age)
+
+
+// utility types
+// interface Users{
+//     name : string,
+//     age : number
+// }
+
+// type Person = Partial<Users>
+// const person : Person={name : "anaida"}
+// console.log(person)
